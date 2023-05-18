2f3cadab13bc7940ed2953ccf3057fd92c9e9bcb
Anxo Campos <anxo.campos260204@gmail.com>
2023-05-18 10:13:10 +0200

    all

---
+++ 3ªEV/MySQL/ejercicios/Practica13.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS productos(
+	id INT auto_increment primary key,
+    descripcion VARCHAR(50),
+    precio DECIMAL(10,2));
---
+++ 3ªEV/MySQL/ejercicios/Practica14.sql
@@ -0,0 +1,2 @@
+ALTER TABLE productos CHANGE descicion  descripcion VARCHAR(50); 
+INSERT INTO productos (Descripcion, precio) VALUES ("Cable mod. Eloy", 5.23), ("Pañuelos Recamán",2.0),("Cases Eloy, 7.5")
